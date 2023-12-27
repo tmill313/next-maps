75a048e75578bdbe109b85962e36d8385ce3fca4
Nikki <nikki@Nikkis-MacBook-Air.local>
2023-12-26 21:30:00 -0700

    stripe config

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const config = {
         priceId:
           process.env.NODE_ENV === "development"
             ? "price_1ORo19D7etE5Yet8mHDMhu96"
-            : "price_456",
+            : "price_1ORo19D7etE5Yet8mHDMhu96",
         //  REQUIRED - Name of the plan, displayed on the pricing page
         name: "Starter",
         // A friendly description of the plan, displayed on the pricing page. Tip: explain why this plan and not others
