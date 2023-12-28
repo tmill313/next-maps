d8e307b2fbec06624063e34752d3b7dcb5ce0b2a
Nikki <nikki@Nikkis-MacBook-Air.local>
2023-12-27 22:36:06 -0700

    salesforce auth

---
+++ app/api/salesforce/route.js
@@ -0,0 +1,95 @@
+import { NextResponse } from "next/server";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import config from "@/config";
+import qs from 'qs';
+import axios from "axios";
+
+// This route is used to store the leads that are generated from the landing page.
+// The API call is initiated by <ButtonLead /> component
+export async function GET(req) {
+    const thisConfig = config
+    try {
+        const cookieStore = cookies();
+        const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+        const requestUrl = new URL(req.url);
+        const tempCode = requestUrl.searchParams.get("code");
+        const code = decodeURIComponent(tempCode)
+        if(!code) {
+            return NextResponse.json(
+                {
+                  error:
+                    "Auth code is missing",
+                },
+                { status: 400 }
+              );
+        }
+    
+        const {
+          data: { session },
+        } = await supabase.auth.getSession();
+
+            // Search for a profile with unique ID equals to the user session ID (in table called 'profiles')
+    const { data } = await supabase
+    .from("salesforce_auth")
+    .select("*")
+    .eq("id", session?.user?.id)
+    .single();
+
+    if (!data) {
+        await supabase.from("salesforce_auth").insert([
+          {
+            id: session.user.id,
+            auth_code: code
+          },
+        ]);
+      }
+
+let thisData = qs.stringify({
+  'code': code,
+  'grant_type': 'authorization_code',
+  'Content_type': 'application/x-www-form-urlencoded',
+  'client_id': process.env.NEXT_PUBLIC_SALESFORCE_CONSUMER_KEY,
+  'client_secret': process.env.NEXT_PUBLIC_SALESFORCE_CONSUMER_SECRET,
+  'redirect_uri': `${requestUrl.origin}/api/salesforce`
+});
+
+
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://login.salesforce.com/services/oauth2/token',
+  headers: { 
+    'Content-Type': 'application/x-www-form-urlencoded', 
+    'Cookie': 'BrowserId=wGR2SqIbEe6HWqd8LP5WFA; CookieConsentPolicy=0:0; LSKey-c$CookieConsentPolicy=0:0'
+  },
+  data : thisData
+};
+console.log(config)
+
+axios.post('https://login.salesforce.com/services/oauth2/token', thisData, {  headers: { 
+    'Content-Type': 'application/x-www-form-urlencoded', 
+    'Cookie': 'BrowserId=wGR2SqIbEe6HWqd8LP5WFA; CookieConsentPolicy=0:0; LSKey-c$CookieConsentPolicy=0:0'
+  }})
+.then( async (response) => {
+    const data = response.data
+  console.log(data);
+  const {error} = await supabase
+  .from("salesforce_auth")
+  .update({
+    access_token: data.access_token
+  })
+  .eq("id", session?.user?.id);
+  console.log(error)
+})
+.catch((error) => {
+  console.log(error);
+});
+
+return NextResponse.redirect(requestUrl.origin + thisConfig.auth.callbackUrl);
+} catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: e?.message }, { status: 500 });
+}
+
+}
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,10 @@
 'use client'
 import { useState, useEffect } from "react";
+import axios from "axios";
+import Link from "next/link"
 import ButtonAccount from "@/components/ButtonAccount";
 import ButtonCheckout from "@/components/ButtonCheckout";
+import ButtonGradient from "@/components/ButtonGradient";
 import config from "@/config";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import GoogleMap from "@/components/GoogleMap";
@@ -12,12 +15,14 @@ export default function Dashboard() {
   const supabase = createClientComponentClient();
   const [user, setUser] = useState(null);
   const [profile, setProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(true)
 
 
 
   useEffect(() => {
     const getUser = async () => {
-      const { data } = await supabase.auth.getUser()
+      setIsLoading(true)
+      const { loading: userLoading, data } = await supabase.auth.getUser()
       setUser(data.user);
     };
 
@@ -26,19 +31,45 @@ export default function Dashboard() {
 
   useEffect(() => {
     const getUser = async () => {
-      console.log(user)
-      const { data: profileData } = await supabase
+      const { loading, data: profileData } = await supabase
       .from("profiles")
-      .select("*")
+      .select(`*, 
+      salesforce_auth!inner (
+        *
+      )`)
       .eq("id", user?.id)
       .single();
       setProfile(profileData)
+      setIsLoading(false)
     };
 if(user) {
     getUser();
 }
   }, [user]);
 
+  const options = {
+    headers: {
+      Authorization: `Bearer ${profile?.salesforce_auth?.access_token}`,
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      'Access-Control-Allow-Headers': '*',
+    },
+  };
+
+  const getStuff = async () => {
+    try {
+      
+      const res = await axios.post('https://taylormiller-dev-ed.develop.my.salesforce.com/services/data/v59.0/sobjects/Opportunity', {}, options);
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  const salesforceLoginURL = `https://login.salesforce.com/services/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_SALESFORCE_CONSUMER_KEY}&redirect_uri=http://localhost:3000/api/salesforce&response_type=code`
+  
+if(isLoading) return
 
   return (
     <main className="min-h-screen p-8 pb-24">
@@ -55,7 +86,17 @@ if(user) {
         />
         </div>
         :
+        !profile?.salesforce_auth?.access_token ? 
+          <div>
+          <Link href={salesforceLoginURL}>
+            <ButtonGradient title='Connect Salesforce'/>
+          </Link>
         <GoogleMap />
+        </div>
+      :
+      <div>
+        <ButtonGradient title='get stuff' onClick={() => getStuff()}/>
+      </div>
         }
       </section>
     </main>
